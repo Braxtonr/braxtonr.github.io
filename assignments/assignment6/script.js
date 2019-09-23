500ef32d908517ccd8a8f6c14cebfe0ccf9cc294
braxtonr <braxtonrobinson9@gmail.com>
2019-09-23 14:16:02 -0400

    Ass. 6 NON FINAL

---
+++ assignments/assignment6/script.js
@@ -0,0 +1,11 @@
+function CalcBeachBall {
+    const fName =  document.getElementById("fName").value;
+    const lName = document.getElementById("lName").value;
+    const pName = document.getElementById("pName").value;
+    const pCount = document.getElementById("pCount").value;
+
+    const resultElem = document.getElementById("printAll")
+}
+
+const btnresult = document.getElementById("totalsbtn");
+btnresult.onclick = CalcBeachBall;
