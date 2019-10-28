5a0c51a549d3b9b0ec0c262c6db133d045bda0df
braxtonr <braxtonrobinson9@gmail.com>
2019-10-28 14:53:37 -0400

    class stuff

---
+++ in_class/days/10-28-19/script.js
@@ -0,0 +1,14 @@
+async function special() {
+    console.log("In Special Beginning");
+    let result = await setTimeout(doStuff, 2000);
+    console.log("In Special Ending");
+}
+
+function doStuff(){
+    console.log("In do stuff");
+}
+window.onload = function(){
+    this.console.log("Before calling special");
+    this.special();
+    this.console.log("After calling special");
+}
