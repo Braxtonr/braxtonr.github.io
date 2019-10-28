7b758eaef0cec1427ab3e2f61020900f8943831f
braxtonr <braxtonrobinson9@gmail.com>
2019-10-28 15:25:23 -0400

    class stuff

--- in_class/days/10-28-19/script.js
+++ in_class/days/10-28-19/script.js
@@ -1,4 +1,4 @@
-async function special() {
+/*async function special() {
     console.log("In Special Beginning");
     let result = await setTimeout(doStuff, 2000);
     console.log("In Special Ending");
@@ -11,4 +11,32 @@ window.onload = function(){
     this.console.log("Before calling special");
     this.special();
     this.console.log("After calling special");
+}*/
+
+async function displayShoe(){
+    let response = await fetch("https://braxtonr.github.io/in_class/days/10-28-19/shoe.json");
+    let shoeJSon = await response.json();
+    let shoeName = shoeJSon.name;
+    let shoeBrand = shoeJSon.brand;
+    let reviews = shoeJSon.comments;
+    console.log(`${shoeName} by ${shoeBrand}`);
+ 
+    for(i in reviews) {
+        console.log(reviews[i]);
+    }
+
+    let section = document.createElement("section");
+    section.innerHTML = (`${shoeName} by ${shoeBrand}`);
+    let content = document.getElementById("content");
+    content.append(section);
+
+}
+
+window.onload = function(){
+    this.displayShoe();
+
+    let content  = document.getElementById("content");
+    let h1 = document.createElement("h1");
+    h1.innerHTML = "I love your shoes";
+    content.append(h1);
 }
